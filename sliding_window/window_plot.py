4ab06a34def21cd15ef87aa85fc40a3af522643a
stillahill <elliot.douglas.hill@gmail.com>
2021-08-30 17:40:54 -0500

    Refactor all plots from seaborn/matplotlib to bokeh

--- sliding_window/window_plot.py
+++ sliding_window/window_plot.py
@@ -1,7 +1,10 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
-from matplotlib.widgets import Slider
+from scipy.stats.kde import gaussian_kde
+from bokeh.io import show
+from bokeh.layouts import row, gridplot
+from bokeh.models import RangeTool, FuncTickFormatter, Div
+from bokeh.plotting import figure, output_file
+from bokeh.palettes import Category20
 
 
 class WindowPlot:
@@ -11,164 +14,171 @@ class WindowPlot:
         self.colors = colors
         # helper variables
         self.plots_path = 'results/plots/'
-        self.filtered_range = self.calc_window_range(
-            self.sw.window_data_filtered)
-        self.unfiltered_range = self.calc_window_range(self.sw.window_data)
-        # add x-axis tick positions to data
-        self.add_x_ticks(self.sw.window_data_filtered, self.filtered_range)
-        self.add_x_ticks(self.sw.window_data, self.unfiltered_range)
-        self.dot_plot_title = self.make_title()
-        self.alighment_length = self.sw.summary_stats['alignment_length'][0]
+        self.groupby_subset = self.sw.window_data.groupby('subset')
+        self.groupby_subset_filtered = self.sw.window_data_filtered.groupby(
+            'subset')
+        self.parameter_text = self.make_parameter_text()
+        # append x-ticks for dot plots
+        self.add_x_ticks(self.sw.window_data)
+        self.add_x_ticks(self.sw.window_data_filtered)
+
+    def make_parameter_text(self):
+        target_text = ', '.join(self.sw.target)
+        window_size_text = f"Window size: {self.sw.window_size}"
+        is_plural = 's' if self.sw.target.size > 1 else ''
+        stride_text = f"Window stride: {self.sw.stride}"
+        return f"Target{is_plural}: {target_text}</br>{window_size_text}</br>{stride_text}"
 
     def num_windows(self, df):
         return df.shape[0] / self.sw.n_subset
 
     def calc_window_range(self, df):
-        return np.arange(self.num_windows(df))
+        return np.arange(1, self.num_windows(df)+1)
 
-    def add_x_ticks(self, df, window_range):
-        df['x_ticks'] = np.repeat(window_range, self.sw.n_subset)
-
-    def save_plot(self, file_name):
-        plt.savefig(f'{self.plots_path}{file_name}.png')
-        plt.savefig(f'{self.plots_path}{file_name}.pdf')
+    def add_x_ticks(self, df):
+        df['x_ticks'] = np.repeat(self.calc_window_range(
+            df), self.sw.n_subset)
 
     def ecdf_plot(self):
-        plot = sns.displot(
-            data=self.sw.window_data,
-            x="window_mean",
-            hue="subset",
-            kind="ecdf",
-            palette=self.colors)
-        plot._legend.set_title('Subset')
-        plt.xlabel('Mean target frequency')
-        plt.tight_layout()
-        self.save_plot('ecdf_plot')
-        plt.show()
+        p = figure(x_axis_label='Window frequency mean',
+                   y_axis_label='Fraction of data')
+        for name, group in self.groupby_subset:
+            x = np.sort(group['window_mean'])
+            y = np.arange(len(x))/float(len(x))
+            color = group['color'].values[0]
+            p.step(x, y, line_width=2, mode="center",
+                   color=color, muted_color=color,
+                   muted_alpha=0.2, legend_label=name)
+        p.legend.click_policy = "hide"
+        p.legend.location = "bottom_right"
+        return p
 
     def kde_plot(self):
-        max_freq = self.sw.window_data['window_mean'].max()
-        plot = sns.displot(
-            data=self.sw.window_data,
-            x="window_mean",
-            hue="subset",
-            kind="kde",
-            common_norm=False,
-            palette=self.colors).set(xlim=(0, max_freq))
-        plot._legend.set_title('Subset')
-        plt.xlabel('Mean target frequency')
-        plt.tight_layout()
-        self.save_plot('kde_plot')
-        plt.show()
+        maxs = self.groupby_subset['window_mean'].max()
+        mins = self.groupby_subset['window_mean'].min()
+        p = figure(x_axis_label='Window frequency mean',
+                   y_axis_label='Density', x_range=(np.min(mins), np.max(maxs)))
+        for name, group in self.groupby_subset:
+            frequencies = group['window_mean']
+            x = np.linspace(mins.loc[name], maxs.loc[name], 1000)
+            pdf = gaussian_kde(frequencies)
+            y = pdf(x)
+            color = group['color'].values[0]
+            p.line(x, y, legend_label=name, line_width=2,
+                   line_color=color, muted_color=color,
+                   muted_alpha=0.2)
+            p.legend.click_policy = "hide"
+        return p
 
-    def make_title(self):
-        target_text = ', '.join(self.sw.target)
-        window_size_text = f"\tWindow size: {self.sw.window_size}".expandtabs()
-        is_plural = 's' if self.sw.target.size > 1 else ''
-        stride_text = f"\tWindow stride: {self.sw.stride}".expandtabs()
-        return f"Target{is_plural}: {target_text}{window_size_text}{stride_text}"
+    def distribution_plots(self):
+        output_file("results/plots/distributions.html",
+                    title="Sliding window plot")
+        p = self.ecdf_plot()
+        q = self.kde_plot()
+        show(row(p, q))
 
-    def format_x_ticks(self, axes, window_range, filtered):
-        axes.tick_params(
-            axis='x',
-            which='major',
-            labelsize=6,
-            labelrotation=-90)
-        axes.set_xticks(window_range)
-        if filtered:
-            x_tick_labels = self.sw.window_data_filtered['window'].unique()
+    def make_color_palette(self):
+        colors = Category20[20]
+        color_map = {name: color for name,
+                     color in zip(self.sw.subset_names, colors)}
+        return self.sw.window_data['subset'].map(color_map)
+
+    def dot_plot(self, groupby_subset, tools, x_range):
+        tooltips = [
+            ("Subset", "@subset"),
+            ("Window position", "@window_start"),
+            ("Mean frequency", "@window_mean"),
+        ]
+        p = figure(
+            plot_height=400,
+            plot_width=1000,
+            tools=tools,
+            toolbar_location="right",
+            x_axis_location="below",
+            background_fill_color="#efefef",
+            x_range=x_range,
+            tooltips=tooltips)
+
+        for name, group in groupby_subset:
+            p.segment('x_ticks', 0, 'x_ticks', 'window_mean', line_color='#bdbdbd',
+                      line_width=1, legend_label=name, source=group)
+        circles = []
+        for name, group in groupby_subset:
+            circles.append(p.circle('x_ticks', 'window_mean',
+                                    size=7,
+                                    legend_label=name,
+                                    color='color',
+                                    muted_color='color',
+                                    muted_alpha=0.2,
+                                    source=group))
+        p.legend.click_policy = "hide"
+        p.hover.renderers = circles
+        p.yaxis.axis_label = 'Window frequency mean'
+        return p
+
+    def filtered_dot_plot(self, df):
+        output_file("results/plots/filtered_dot_plot.html",
+                    title="Sliding window plot")
+        if self.sw.n_largest > 50:
+            x_range = (0, 50)
         else:
-            x_tick_labels = self.sw.window_data['window'].unique(
-            ) * self.sw.stride
-        axes.set_xticklabels(x_tick_labels)
+            x_range = (0, self.sw.n_largest+1)
+        p = self.dot_plot(self.groupby_subset_filtered,
+                          ['xpan', 'save'], x_range)
+        p.xaxis.axis_label = 'Window position'
+        p.xaxis.ticker = df['x_ticks']
+        self.format_x_axis(p, df, 'x_ticks', 'window_start')
+        p.xaxis.major_label_orientation = "vertical"
+        div = Div(text=self.parameter_text, width=200, height=100)
+        show(row(p, div))
 
-    def scatter_plot(self, df, axes, alpha):
-        axes = sns.scatterplot(
-            data=df,
-            x='x_ticks',
-            y='window_mean',
-            hue='subset',
-            palette=self.colors,
-            linewidth=0,
-            ax=axes,
-            alpha=alpha)
-        axes.legend(title='Subset')
-        axes.set(xlabel=None)
-        axes.set(ylabel=None)
-        plt.tight_layout()
-        return axes
+    def format_x_axis(self, p, df, ticks, tick_labels):
+        x_label = {key: value for key, value in zip(
+            df[ticks], df[tick_labels])}
+        p.xaxis.formatter = FuncTickFormatter(code="""
+            var labels = %s;
+            return labels[tick] || tick;
+            """ % x_label)
 
-    def dot_plot(self, df, window_range, filtered, axes=None, save_plot=True, show=True):
-        plt.rcParams['figure.figsize'] = [15, 6]
-        label = "Window"
-        groupby_window = self.sw.groupby_window
-        if not axes:
-            label = f"Filtered window"
-            groupby_window = df.groupby('window')
-        axes = self.scatter_plot(df, axes, 1)
-        axes.legend(title='Subset')
-        min_values = groupby_window.min()['window_mean']
-        max_values = groupby_window.max()['window_mean']
-        _, y_max = axes.get_ylim()
-        plt.ylim((min_values.min(), y_max))
-        plt.vlines(x=window_range, ymin=-0.1,
-                   ymax=max_values, color='grey', zorder=0, lw=0.6)
-        self.format_x_ticks(axes, window_range, filtered)
-        plt.title(self.dot_plot_title)
-        x_label = f'{label} position'
-        plt.xlabel(x_label, labelpad=10)
-        plt.ylabel('Mean target frequency')
-        plt.tight_layout()
-        if save_plot:
-            self.save_plot('dot_plot_filtered')
-        if show:
-            plt.show()
+    def sliding_panel(self, p, df, groupby_subset):
+        self.format_x_axis(p, df, 'x_ticks', 'window_start')
+        select = figure(
+            plot_height=150,
+            plot_width=1000,
+            y_range=p.y_range,
+            y_axis_type=None,
+            tools="",
+            toolbar_location=None,
+            background_fill_color="#efefef"
+        )
+        select.xaxis.axis_label = 'Window position'
+        self.format_x_axis(select, df, 'x_ticks', 'window_start')
+        range_tool = RangeTool(x_range=p.x_range)
+        range_tool.overlay.fill_color = "navy"
+        range_tool.overlay.fill_alpha = 0.2
+        for name, group in groupby_subset:
+            select.circle('x_ticks', 'window_mean',
+                          color=group['color'].values[0], alpha=0.6, source=group)
+        select.ygrid.grid_line_color = None
+        select.add_tools(range_tool)
+        select.toolbar.active_multi = range_tool
+        return select
 
-    # convert a scalar from one range to another
-    def interpolate_range(self, value, old_xticks):
-        return int(np.interp(value, [old_xticks[1], old_xticks[-2]], [1, self.alighment_length]))
-
-    # map window stride range to alighnment length range for x ticks
-    def reset_xticks(self, axes):
-        old_xticks = axes[0].get_xticks()
-        axes[0].set_xticks(old_xticks[1:-1])
-        new_xticks = [self.interpolate_range(value, old_xticks)
-                      for value in old_xticks]
-        axes[0].set_xticklabels(new_xticks[1:-1])
-
-    def sliding_plot(self, plot_scroll_size):
-        fig, axes = plt.subplots(2)
-        self.dot_plot(self.sw.window_data, self.unfiltered_range, filtered=False,
-                      axes=axes[1], save_plot=False, show=False)
-        axes[0] = self.scatter_plot(self.sw.window_data, axes[0], 0.6)
-        axes[1].set(title=None)
-        axes[1].set(ylabel=None)
-        axes[0].set(title=self.dot_plot_title)
-        self.reset_xticks(axes)
-        plt.subplots_adjust(left=0.15, right=0.9, bottom=0.2, top=0.90)
-        # add common y label
-        fig.text(0.05, 0.5, 'Mean target frequency', va='center',
-                 ha='center', rotation='vertical')
-        # set the axis and slider position in the plot
-        axis_pos = plt.axes([0.15, 0.01, 0.75, 0.03], facecolor='White')
-        slider_pos = Slider(axis_pos, 'Window', 0.0,
-                            self.num_windows(self.sw.window_data), valinit=0)
-        # update() function to change the graph when the slider is in use
-        y_min, y_max = axes[1].get_ylim()
-
-        def update(val):
-            pos = int(slider_pos.val) + 1
-            axes[1].axis([pos - 1, pos + plot_scroll_size, y_min, y_max])
-            slider_pos.valtext.set_text('{}'.format(pos))
-            fig.canvas.draw_idle()
-
-        update(slider_pos.val)
-        slider_pos.on_changed(update)
-        plt.show()
+    def sliding_window_plot(self, df):
+        output_file("results/plots/dot_plot.html",
+                    title="Sliding window plot")
+        initial_panel_size = int(
+            self.sw.summary_stats['alignment_length'][0] * 0.1)
+        p = self.dot_plot(self.groupby_subset, [
+                          "xpan", "save"], (0, initial_panel_size))
+        select = self.sliding_panel(p, df, self.groupby_subset)
+        div = Div(text=self.parameter_text, width=200, height=100)
+        grid = gridplot([[p, div], [select, None]])
+        show(grid)
 
     def make_plots(self):
-        self.sliding_plot(plot_scroll_size=50)
-        self.dot_plot(self.sw.window_data_filtered,
-                      self.filtered_range, filtered=True)
-        self.kde_plot()
-        self.ecdf_plot()
+        self.sw.window_data['color'] = self.make_color_palette()
+        self.sw.window_data_filtered['color'] = self.make_color_palette()
+        self.sliding_window_plot(self.sw.window_data)
+        self.filtered_dot_plot(self.sw.window_data_filtered)
+        self.distribution_plots()
